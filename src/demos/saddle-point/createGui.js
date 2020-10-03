44374e2ae3cb6ee270d2c533dc7d031ca7fef0c5
Einar Persson <einar.jpg@gmail.com>
2020-10-03 18:28:21 +0200

    added gui class for standard controls

--- src/demos/saddle-point/createGui.js
+++ src/demos/saddle-point/createGui.js
@@ -1,77 +1,21 @@
-import dat from "three/examples/jsm/libs/dat.gui.module";
+import Gui from "../../dynamic-relaxation/gui";
 
-export default function createGui(solver, startVertices, updateGeometry) {
-  function resetVertices() {
-    solver.vertices = startVertices;
-    solver.velocities = startVertices.map(() => [0, 0, 0]);
-    solver.energy.pop();
-    solver.energy.unshift(0);
-    solver.iterationCount = 0;
-    updateGeometry(startVertices);
-  }
+export default function createGui(solver, updateGeometry) {
+  let gui = new Gui(solver, updateGeometry);
 
-  class GuiObj {
-    constructor() {
-      this.run = true;
-      this.reset = () => {
-        resetVertices();
-        if (this.run) {
-          solver.resumeSimulation();
-        }
-      };
-    }
-  }
-  const guiObj = new GuiObj();
-
-  let gui = new dat.GUI();
-  let solverGui = gui.addFolder("Solver");
-  solverGui.open();
-
-  let controller = solverGui.add(guiObj, "run");
-  controller.onFinishChange((value) => {
-    if (guiObj.run) {
-      solver.resumeSimulation();
-    } else {
-      solver.stopSimulation();
-    }
-  });
-
-  solverGui.add(guiObj, "reset");
-
-  let statusController = solverGui.add(solver, "status").name("solver status");
-  statusController.domElement.style.pointerEvents = "none";
-
-  let iterationController = solverGui
-    .add(solver, "iterationCount")
-    .name("iteration");
-  iterationController.listen();
-  iterationController.domElement.style.pointerEvents = "none";
-  solver.onStatusChange(() => {
-    statusController.updateDisplay();
-  });
-
-  // Force Goal gui
   const forceGoal = solver.goals[solver.goals.length - 1];
-  createForceGoalGui(gui, guiObj, solver, forceGoal);
-}
+  function resume() {
+    if (gui.run) {
+      solver.resumeSimulation();
+    }
+  }
 
-function createForceGoalGui(gui, guiObj, solver, forceGoal) {
-  let forceGoalGui = gui.addFolder("Force Goal");
-  forceGoalGui.open();
-
-  let controller = forceGoalGui
+  let forceGoalFolder = gui.addFolder("Force Goal");
+  forceGoalFolder.open();
+  forceGoalFolder
     .add(forceGoal, "vertexIndex", 0, solver.vertices.length - 1)
-    .step(1);
-  controller.onFinishChange(() => {
-    if (guiObj.run) {
-      solver.resumeSimulation();
-    }
-  });
+    .step(1)
+    .onFinishChange(resume);
 
-  controller = forceGoalGui.add(forceGoal, "strength", -2, 2);
-  controller.onFinishChange(() => {
-    if (guiObj.run) {
-      solver.resumeSimulation();
-    }
-  });
+  forceGoalFolder.add(forceGoal, "strength", -2, 2).onFinishChange(resume);
 }
---
+++ src/dynamic-relaxation/gui.js
@@ -0,0 +1,68 @@
+import dat from "three/examples/jsm/libs/dat.gui.module";
+
+export default class Gui {
+  constructor(solver, updateGeometry) {
+    this._gui = new dat.GUI();
+    this._solver = solver;
+    this._startVertices = solver.vertices.slice();
+    this._updateGeometry = updateGeometry;
+
+    let solverFolder = this._gui.addFolder("Solver");
+    solverFolder.open();
+    this._addRunToggle(solverFolder);
+    this._addResetButton(solverFolder);
+    this._addIterationCount(solverFolder);
+    this._addSolverStatus(solverFolder);
+  }
+
+  addFolder(name) {
+    return this._gui.addFolder(name);
+  }
+
+  _addRunToggle(guiFolder) {
+    this.run = true;
+    let controller = guiFolder.add(this, "run");
+    controller.onFinishChange((value) => {
+      console.log(value);
+      if (this.run) {
+        this._solver.resumeSimulation();
+      } else {
+        this._solver.stopSimulation();
+      }
+    });
+  }
+
+  _addResetButton(guiFolder) {
+    let controller = guiFolder.add(this, "_reset").name("reset");
+  }
+
+  _addIterationCount(guiFolder) {
+    let controller = guiFolder
+      .add(this._solver, "iterationCount")
+      .name("iteration");
+    controller.listen();
+    controller.domElement.style.pointerEvents = "none";
+  }
+
+  _addSolverStatus(guiFolder) {
+    let controller = guiFolder
+      .add(this._solver, "status")
+      .name("solver status");
+    controller.domElement.style.pointerEvents = "none";
+    this._solver.onStatusChange(() => {
+      controller.updateDisplay();
+    });
+  }
+
+  _reset() {
+    this._solver.vertices = this._startVertices;
+    this._solver.velocities = this._startVertices.map(() => [0, 0, 0]);
+    this._solver.energy.pop();
+    this._solver.energy.unshift(0);
+    this._solver.iterationCount = 0;
+    this._updateGeometry(this._startVertices);
+    if (this.run) {
+      this._solver.resumeSimulation();
+    }
+  }
+}
